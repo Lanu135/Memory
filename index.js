59400d643e702d27d98247cf5f1ede3e51297ce4
Lanu135 <143382856+Lanu135@users.noreply.github.com>
2024-05-06 15:28:01 +0200

    added cards array and also functionalty of the game

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
 const cards = [
   {
     name: 'apple',
-    img: './images/apple.jpg',
+    src: './images/apple.jpg',
+    alt: 'apple',
+    isFlipped: false,
   },
   {
     name: 'bread',
-    img: './images/bread.jpg',
+    src: './images/bread.jpg',
+    alt: 'bread',
+    isFlipped: false,
   },
   {
     name: 'broccoli',
-    img: './images/broccoli.jpg',
+    src: './images/broccoli.jpg',
+    alt: 'broccoli',
+    isFlipped: false,
   },
   {
     name: 'coffee',
-    img: './images/coffee.jpg',
+    src: './images/coffee.jpg',
+    alt: 'coffee',
+    isFlipped: false,
   },
   {
     name: 'donut',
-    img: './images/donut.jpg',
+    src: './images/donut.jpg',
+    alt: 'donut',
+    isFlipped: false,
   },
   {
     name: 'meat',
-    img: './images/meat.jpg',
+    src: './images/meat.jpg',
+    alt: 'meat',
+    isFlipped: false,
   },
   {
     name: 'milk',
-    img: './images/milk.jpg',
+    src: './images/milk.jpg',
+    alt: 'milk',
+    isFlipped: false,
   },
   {
     name: 'pizza',
-    img: './images/pizza.jpg',
+    src: './images/pizza.jpg',
+    alt: 'pizza',
+    isFlipped: false,
   },
   {
     name: 'squit',
-    img: './images/squit.jpg',
+    src: './images/squit.jpg',
+    alt: 'squit',
+    isFlipped: false,
+  },
+  {
+    name: 'peanut',
+    src: './images/peanut.jpg',
+    alt: 'peanut',
+    isFlipped: false,
   },
 ]
 
@@ -42,6 +66,10 @@ cards.push(...cards)
 cards.sort(() => 0.5 - Math.random())
 
 const cardsBoard = document.querySelector('.cards-board')
+let score = 0
+
+const chosenCards = []
+const chosenCardsIds = []
 
 function createBoard() {
   let i = 0
@@ -49,10 +77,50 @@ function createBoard() {
     const img = document.createElement('img')
     img.setAttribute('src', './images/cards-bg.jpg')
     img.setAttribute('data-id', i)
-    console.log(img)
     i++
+    img.addEventListener('click', flipCard)
     cardsBoard.appendChild(img)
   })
 }
 
 createBoard()
+
+// flips the cards and put the flipped Cards into a new array
+function flipCard() {
+  const cardId = this.getAttribute('data-id')
+  this.setAttribute('src', cards[cardId].src)
+  chosenCards.push(cards[cardId].name)
+  chosenCardsIds.push(cardId)
+  if (chosenCards.length === 2) {
+    setTimeout(checkMatch, 500)
+  }
+}
+
+function checkMatch() {
+  const scoreElement = document.getElementById('score')
+  const img = document.querySelectorAll('img')
+  if (chosenCardsIds[0] === chosenCardsIds[1]) {
+    alert('You clicked the same card, please choose two different')
+    img[chosenCardsIds[0]].setAttribute('src', './images/cards-bg.jpg')
+    img[chosenCardsIds[1]].setAttribute('src', './images/cards-bg.jpg')
+    chosenCards.splice(0, 2)
+    chosenCardsIds.splice(0, 2)
+  } else if (chosenCards[0] == chosenCards[1]) {
+    score++
+    scoreElement.innerText = score
+    // cannot be added to the chosenCards Array and cursor we be set to default
+    chosenCardsIds.forEach((id) => {
+      img[id].removeEventListener('click', flipCard)
+      img[id].setAttribute('src', './images/clear.jpeg')
+      img[id].style.cursor = 'default'
+      img[id].style.border = 'none'
+    })
+  } else {
+    // flip back the card if is not a match
+    chosenCardsIds.forEach((id) => {
+      img[id].setAttribute('src', './images/cards-bg.jpg')
+    })
+  }
+  chosenCards.splice(0, 2)
+  chosenCardsIds.splice(0, 2)
+}
